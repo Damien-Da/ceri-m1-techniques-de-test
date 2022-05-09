3d97aba8499e69a11b425030115388d829aa9012
Damien Dallon <damien.dallon@alumni.univ-avignon.fr>
2022-05-09 10:23:22 +0200

    Modification javadoc

--- src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
@@ -1,5 +1,9 @@
 package fr.univavignon.pokedex.api;
 
+/**
+ * Pokedex builder
+ * @author damien dallon
+ */
 public class PokedexFactory implements IPokedexFactory {
     @Override
     public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
